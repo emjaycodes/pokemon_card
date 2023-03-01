e2554c5bfdc7181ce1c724b34c8da27dc8b416f0
jaycodes <justicenwogu8@gmail.com>
2023-03-01 10:21:54 +0100

    commit

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,12 +1,12 @@
 // ignore_for_file: avoid_print
+import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:pokemon_card/models/pokemon.dart';
 
 
-Future<PokemonInfo> getpokemondata(String query) async {
-    
-    query = 'pichu';
+Future<PokemonInfo> getpokemondata() async {
+  int query = 6;
     String url = 'https://pokeapi.co/api/v2/pokemon/$query';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
@@ -17,3 +17,4 @@ Future<PokemonInfo> getpokemondata(String query) async {
         throw (response.statusCode);
     }
 }
+
