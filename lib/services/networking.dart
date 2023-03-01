b05ccbbd85aa6e948b6baaefdf081a4f0eb45d5f
jaycodes <justicenwogu8@gmail.com>
2023-03-01 18:16:32 +0100

    change to character id

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: avoid_print
 import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:pokemon_card/models/pokemon.dart';
 
 
-Future<PokemonInfo> getpokemondata() async {
-  int query = 6;
-    String url = 'https://pokeapi.co/api/v2/pokemon/$query';
+
+Future<PokemonInfo> getpokemondata(int characterid) async {
+    String url = 'https://pokeapi.co/api/v2/pokemon/$characterid';
     final response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
         String data = response.body;
